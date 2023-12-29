c1c481193e1a6d84c03508f78b3460cf360febd4
Philodev <zimilifila3676@gmail.com>
2023-12-29 03:24:19 -0800

    media

--- frontend/src/components/TopNav/TopNav.jsx
+++ frontend/src/components/TopNav/TopNav.jsx
@@ -2,10 +2,12 @@
 import React from 'react'
 import './TopNav.css'
 import logo from '../../img/logo.png'
+import de from '../../img/de.png'
 function TopNav() {
   return (<>
     <div className='top-nav'>
     <img className='logo' src={logo} alt="logo" />
+    <img className='mini-logo' src={de} alt="logo" />
     </div>
     </>
   )
