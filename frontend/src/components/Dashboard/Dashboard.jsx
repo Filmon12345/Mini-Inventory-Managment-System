37db7038c828a864118094eb5cb4c729d96a9c5e
Philodev <zimilifila3676@gmail.com>
2024-01-17 00:28:47 -0800

    Add table to dashboard page

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -7,33 +7,25 @@ import Cost from "../../img/Cost.png";
 import "./Dashboard.css";
 
 function Dashboard() {
-  const [search, Setsearch] = useState();
-  const [searchResult, SetsearchResult] = useState([]);
-  const [navparams, Setnavparams] = useState("");
-  const [data, Setdata] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:8000/products/get", {
-      method: "GET",
-    })
-      .then((res) => res.json())
-      .then((products) => {
-        // console.log(products);===>is to check fetch method
+
+
+  const [data,Setdata] = useState([]);
+
+   useEffect(()=>{
+      fetch('http://localhost:8000/products/get',{
+        method: 'GET'
+      })
+      .then((res)=>res.json())
+      .then((products)=>{
         Setdata(products);
       });
+  
+  
+   
+   },);
 
-    try {
-      fetch(`http://localhost:8000/products/search/${search}`, {
-        method: "GET",
-      })
-        .then((res) => res.json())
-        .then((product) => {
-          SetsearchResult(product);
-        });
-    } catch (err) {
-      console.log(err.message);
-    }
-  }, [search]);
+   
   const totalCost = data.reduce((acc, i) => {
     const productcost = i.price * i.amount;
     return acc + productcost;
@@ -82,96 +74,34 @@ function Dashboard() {
           </div>
         </div>
         <hr />
-        <div className="dashboard-container">
-          {searchResult.map((i, index) => {
-            const cardstatus = i.amount > 40 ? "SUFFICIENT" : "INSUFFICIENT";
-            const status = i.amount > 40 ? "SUFFICIENT" : "INSUFFICIENT";
-            return (
-              <div key={index} className="card">
-                <div className={`status ${cardstatus}`}>{status}</div>
+        <div className={` tableContainer`}>
+      <table className='summary-table'>
+     <thead>
+          <tr>
+          <th>Product Name</th>
+          <th>Category</th>
+          <th>Amount</th>
+          <th>Price</th>
+          <th>Status</th>
+          </tr>
+          </thead>
+         { data.slice().reverse().map((i,index)=>{
+          const cardstatus =i.amount>40?' Sufficient':'Insuficient';
+         return(
+          <tbody key={index}>
+            <tr>
+            <td>{i.name}</td>
+            <td>{i.category}</td>
+            <td>{i.amount}</td>
+            <td>{i.price}</td>
+            <td className= { `${cardstatus}`}>{cardstatus}</td>
+            </tr>
+          </tbody>
+         )})
+          }      
+        </table>
+    </div>
 
-                <div className="imgs">
-                  <img className="cards-img" src={i.image} alt="" />
-                </div>
-                <div className="product-detail">
-                  <h3>{i.name}</h3>
-                  <h4>Category: {i.category}</h4>
-                  <p className="price">Price: {i.price} Birr</p>
-                  <p className="Amount">Amount: {i.amount}</p>
-                  <p className="description">{i.description}</p>
-                </div>
-              </div>
-            );
-          })}
-          <div className="category-list">
-            <a
-              className="catagory"
-              href={`/see/${navparams}`}
-              onClick={() => {
-                Setnavparams("Electronics");
-              }}
-            >
-              {" "}
-              <div>
-                <h1 className="title">Electronics</h1>
-              </div>
-            </a>
-            <a
-              className="catagory"
-              href={`/see/${navparams}`}
-              onClick={() => {
-                Setnavparams("Mechanical Tools");
-              }}
-            >
-              {" "}
-              <div>
-                <div>
-                  <h1 className="title">Mechanical Tools</h1>
-                </div>
-              </div>
-            </a>
-            <a
-              className="catagory"
-              href={`/see/${navparams}`}
-              onClick={() => {
-                Setnavparams("Electrical Tools");
-              }}
-            >
-              {" "}
-              <div>
-                <div className="  ">
-                  <h1 className="title">Electrical Tools</h1>
-                </div>
-              </div>
-            </a>
-            <a
-              className="catagory"
-              href={`/see/${navparams}`}
-              onClick={() => {
-                Setnavparams("Clothes");
-              }}
-            >
-              {" "}
-              <div>
-                <div>
-                  <h1 className="title">Clothes</h1>
-                </div>
-              </div>
-            </a>
-             <a
-              className="catagory"
-              href={`/see/${navparams}`}
-              onClick={() => {
-                Setnavparams("others");
-              }}
-            >
-              {" "}
-              <div>
-                <h1 className="title">Others</h1>
-              </div>
-            </a>
-          </div>
-        </div>
       </div>
     </div>
   );
